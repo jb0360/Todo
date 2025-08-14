e7d26af362680e838b23cddf3ea16e7649726e53
jay talaviya <jay.talaviya@zeuslearning.com>
2025-08-14 19:49:08 +0530

    Add Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,97 @@
+import { useState } from 'react'
 import './App.css'
 import AddTask from './component/AddTask'
 import Card from './component/Card'
 import DeleteTask from './component/DeleteTask'
 import EditTask from './component/EditTask'
-import datas from "./data/data"
+import tasksData from './data/data'
+import type { Task } from './data/data'
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>(tasksData);
+  const [isAddTaskOpen, setIsAddTaskOpen] = useState(false);
+  const [isEditTaskOpen, setIsEditTaskOpen] = useState(false);
+  const [isDeleteTaskOpen, setIsDeleteTaskOpen] = useState(false);
+  const [editingTask, setEditingTask] = useState<Task | null>(null);
+  const [deleteTaskId, setDeleteTaskId] = useState<number | null>(null);
+
+  const addTaskPopup = () =>{
+    setIsAddTaskOpen(prev => !prev);
+  }
+
+  const editTaskPopup = (data: Task | null) =>{
+    setEditingTask(data);    
+    setIsEditTaskOpen(prev => !prev);
+  }
+  // console.log(editingTask);
+  
+
+  const deleteTaskPopup = (dltId: Task['id']) =>{
+    setDeleteTaskId(dltId);
+    setIsDeleteTaskOpen(prev => !prev);
+  }
+
+  const addNewTask = (taskName: string, priority: Task['priority']) => {
+    const newTask: Task = {
+      id: Date.now(),
+      task: taskName,
+      priority,
+      progress: 'To Do'
+    }
+
+    setTasks(prev => [...prev, newTask])
+    setIsAddTaskOpen(false);
+    // console.log("Final-Tasks:",tasks);
+  }
+
+  const editTask = (id: Number,taskName: string, priority: Task['priority'])=>{
+    console.log("id:",id,"taskName", taskName, 'priority:', priority); 
+    setTasks(prev => prev.map(task => task.id !== id ? task : {
+      ...task,
+      task: taskName,
+      priority,
+    }))
+    setIsEditTaskOpen(false);
+  }
+
+  const deleteTask = (dlt: Boolean)=>{
+    // console.log("delete", dlt, 'id:', deleteTaskId); 
+    if (dlt) {
+      setTasks(prev => prev.filter(task=> task.id !== deleteTaskId))
+    }
+    setIsDeleteTaskOpen(false);
+  }
+
   return (
-    <div className='tasklist'>
-      <header className='header'>
-        <div className='logo'>Task List</div>
-        <button className='addtask'>Add Task</button>
-      </header>
-      <div className='card-list'>
-        {
-          datas.map((data: Object)=>{
-            return <Card key={data.id} data={data}/>
-          })
-        }
+    <div className='container'>
+      <div className={isAddTaskOpen || isEditTaskOpen || isDeleteTaskOpen? 'tasklist blurred' : 'tasklist'}>
+        <header className='header'>
+          <div className='logo'>Task List</div>
+          <button className='addtask' onClick={addTaskPopup}>Add Task</button>
+        </header>
+        <div className='card-list'>
+          {
+            tasks.map((data: Task) => {
+              return <Card key={data.id} data={data} editTaskPopup={editTaskPopup} deleteTaskPopup={deleteTaskPopup} />
+            })
+          }
+        </div>
       </div>
-      <AddTask />
-      <DeleteTask />
-      <EditTask />
+      {isAddTaskOpen && (
+        <div className="popup-show">
+          <AddTask addTaskPopup={addTaskPopup} addNewTask={addNewTask}/>
+        </div>
+      )}
+      {isEditTaskOpen && (
+        <div className="popup-show">
+          <EditTask editingTask={editingTask} editTaskPopup={editTaskPopup} editTask={editTask} />
+        </div>
+      )}
+      {isDeleteTaskOpen && (
+        <div className="popup-show">
+          <DeleteTask deleteTask={deleteTask}/>
+        </div>
+      )}
     </div>
   )
 }
--- src/component/AddTask.tsx
+++ src/component/AddTask.tsx
@@ -1,33 +1,50 @@
 import { IoMdClose } from "react-icons/io";
 import "./AddTask.css"
-import { useState } from "react";
+import { useState, type FormEvent } from "react";
+import type { Task } from "../data/data";
 
-function AddTask() {
-    const [select, setSelect] = useState('High');
-    const prioritySelect = (val: String) => {
+type AddProps = {
+    addTaskPopup: ()=>void;
+    addNewTask: (taskName: string, priority: Task["priority"]) =>void;
+}
+
+function AddTask({addTaskPopup, addNewTask}: AddProps) {
+    const [taskVal, setTaskVal] = useState('');
+    const [select, setSelect] = useState<Task["priority"]>('High');
+    function prioritySelect(val: Task['priority']){
         setSelect(val);
     }
+
+    function handleTaskData(e: React.ChangeEvent<HTMLInputElement>) {
+        setTaskVal(e.target.value);
+    }
+
+    function handleFormSubmit(e: FormEvent) {
+        e.preventDefault();
+        addNewTask(taskVal, select);
+    }
+
     return (
         <div className='addTask'>
             <div className="top">
                 <p>Add Task</p>
-                <button className="closeButton"><IoMdClose /></button>
+                <button className="closeButton" onClick={addTaskPopup}><IoMdClose /></button>
             </div>
-            <div className="main">
+            <form className="main" onSubmit={handleFormSubmit}>
                 <div>
                     <p>Task</p>
-                    <input type="text" name="task" placeholder="send article to editor" />
+                    <input type="text" name="task" value={taskVal} onChange={handleTaskData} placeholder="send article to editor" required/>
                 </div>
                 <div>
                     <p>Priority</p>
                     <div className="priorityButtons">
-                        <button onClick={() => prioritySelect('High')} className={select === 'High' ? 'high-selected' : 'high'}>High</button>
-                        <button onClick={() => prioritySelect('Medium')} className={select === 'Medium' ? 'medium-selected' : 'medium'}>Medium</button>
-                        <button onClick={() => prioritySelect('Low')} className={select === 'Low' ? 'low-selected' : 'low'}>Low</button>
+                        <button type="button" onClick={() => prioritySelect('High')} className={select === 'High' ? 'high-selected' : 'high'}>High</button>
+                        <button type="button" onClick={() => prioritySelect('Medium')} className={select === 'Medium' ? 'medium-selected' : 'medium'}>Medium</button>
+                        <button type="button" onClick={() => prioritySelect('Low')} className={select === 'Low' ? 'low-selected' : 'low'}>Low</button>
                     </div>
                 </div>
-            </div>
-            <button className='addButton'>Add</button>
+                <button type="submit" className='addButton'>Add</button>
+            </form>
         </div>
     )
 }
